326ed8cc89c172622346ccbda19585d5616bb98b
MeiyinPU <mei-yin.pu@oclock.school>
2025-03-01 16:12:48 +0100

    update PaymentPage

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
 
 import {
   addPrivateOrder,
   setPrivateOrdersInfo,
   setGroupOrdersInfo,
 } from "../store/reducers/orderSlice.jsx";
-import { selectTotalPrice} from "../store/reducers/orderSlice.jsx";
-import { getPrivateOrdersTotalPrice, //所有私人行程小計
-         getPrivateOrderPrice //單筆私人行程價格
-        
-        } from "../utils/calculatePrice"; 
-
+import { selectTotalPrice } from "../store/reducers/orderSlice.jsx";
+import {
+  getPrivateOrdersTotalPrice, //所有私人行程小計
+  getPrivateOrderPrice, //單筆私人行程價格
+} from "../utils/calculatePrice";
 
 import data from "../data/data.json";
 
@@ -21,7 +21,15 @@ import { HiCreditCard } from "react-icons/hi";
 import { IoIosCheckmarkCircle } from "react-icons/io";
 
 function PaymentPage() {
+  const { id } = useParams();
+  const CardData = data.find((item) => item.id === parseInt(id));
+  console.log(CardData);
+
+  if (!CardData) {
+    return <div>Results not found</div>;
+  }
   const privateOrders = useSelector((state) => state.order.privateOrders || []);
+
 
   const navigate = useNavigate();
   // const handleGoBackClick = (id) => {
@@ -32,17 +40,35 @@ function PaymentPage() {
     console.log(privateOrders);
   };
 
-  const handleConfirmOrderClick = (id) => {
-    navigate(
-      `/search-tourguides/tourguide-profile/${id}/private-trips/payment-success`,
-    );
-  };
-  const { id } = useParams();
-  const CardData = data.find((item) => item.id === parseInt(id));
-  console.log(CardData);
+  const handleConfirmOrderClick = async (id) => {
+    try {
+      const res = await axios.post(
+        `http://localhost:8000/api/${id}/private-orders"`,
+        {
+          adultCount: privateOrders.adultCount,
+          childCount: privateOrders.childCount,
+          selectedDate: privateOrders.selectedDate,
+          selectedSlot: privateOrders.selectedSlot,
+          selectedTheme: privateOrders.seletedTheme,
+          tourguideInfo: privateOrders.tourguideInfo,
+        },
 
-  if (!CardData) {
-    return <div>Results not found</div>;
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+
+      console.log("訂單資料送出成功", res.data);
+      navigate(
+        `/search-tourguides/tourguide-profile/${id}/private-trips/payment-success`,
+      );
+    } catch(error) {
+  
+
+      console.error("訂單資料送出失敗", error.response?.data || error.message);
+    }
   }
 
   const [isPrivateOrderOpen, setPrivateOrderOpen] = useState(false);
@@ -50,11 +76,10 @@ function PaymentPage() {
 
   const totalPrice = useSelector(selectTotalPrice);
 
-
-    // 計算所有私人訂單的小計（數字）
-    const subtotalPrivateOrders = privateOrders.reduce((acc, order) => {
-      return acc + getPrivateOrdersTotalPrice(order);
-    }, 0);
+  // 計算所有私人訂單的小計（數字）
+  const subtotalPrivateOrders = privateOrders.reduce((acc, order) => {
+    return acc + getPrivateOrdersTotalPrice(order);
+  }, 0);
 
   return (
     <>
@@ -142,8 +167,15 @@ function PaymentPage() {
               </label>
 
               <div className="flex flex-col gap-4 rounded-lg border bg-primary-100 px-3 py-2">
-                <div className="flex gap-6">
+                <div className="flex justify-between px-2 py-6">
                   <HiCreditCard />
+                  <h4 className="text-xl font-black text-primary-700">
+                    {" "}
+                    刷卡金額
+                  </h4>
+                  <p className="text-xl font-black text-primary-700">
+                    {subtotalPrivateOrders} €
+                  </p>
                 </div>
                 <label className="form-control w-full">
                   <div className="label">
@@ -218,7 +250,7 @@ function PaymentPage() {
                   className="flex min-w-60 justify-center space-x-20 rounded-3xl bg-primary-700 px-2 py-2 text-white"
                   onClick={() => handleConfirmOrderClick(id)}
                 >
-                  <p className="text-xl">送出資料</p>
+                  <p className="text-xl">確認刷卡</p>
                 </button>
               </div>
             </div>
@@ -240,14 +272,7 @@ function PaymentPage() {
                       <p className="text-base font-normal">私人行程訂單</p>
 
                       <p className="text-end text-xl text-primary-700">
-           
-
-                       
-
-       {subtotalPrivateOrders} €
-
-          
-  
+                        {subtotalPrivateOrders} €
                       </p>
                     </div>
                     <button
@@ -258,66 +283,82 @@ function PaymentPage() {
                     </button>
 
                     {/* 私人行程訂單 Modal */}
-                    {/* {isPrivateOrderOpen && (
+                    {isPrivateOrderOpen && (
                       <div className="fixed inset-0 flex items-center justify-center bg-black/50">
                         <div className="w-4/5 max-w-2xl rounded-lg bg-white p-6">
-                          <h3 className="border-b-4 border-secondary-200 py-4 text-2xl text-secondary-500">
+                          <h3 className="flex justify-between border-b-4 border-secondary-200 text-2xl text-secondary-500">
                             私人行程訂單
+                            <span className="pt-4 text-2xl text-primary-700">
+                              {" "}
+                              小計:
+                              {subtotalPrivateOrders} €
+                              {/* {totalPrice ? `${totalPrice} ` : "價格計算中..."} */}
+                            </span>
                           </h3>
-
-                 
-                          <div className="flex flex-col items-center">
-                            <div className="flex space-y-8 px-8 py-6">
-                              <div className="flex items-center space-x-4">
-                                <img
-                                  src="https://images.unsplash.com/photo-1460794418188-1bb7dba2720d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D"
-                                  alt=""
-                                  className="inline-block h-20 w-20 rounded-full"
-                                />
-                                <p className="text-2xl text-secondary-700">
-                                  {privateOrders.length > 0
-                                    ? privateOrders[privateOrders.length - 1]
-                                        .tourguideInfo.name
-                                    : "Loading..."}
-                                </p>
-                              </div>
-                            </div>
-
-                            <div className="flex flex-col items-start space-y-2">
-                              <p className="text-lg text-grey-950">
-                                {privateOrders.length > 0
-                                  ? privateOrders[privateOrders.length - 1]
-                                      .selectedDate
-                                  : "Loading..."}
-                              </p>
-                              <p className="text-lg text-grey-950">
-                                {privateOrders.length > 0
-                                  ? privateOrders[privateOrders.length - 1]
-                                      .selectedSlot
-                                  : "Loading..."}
-                              </p>
-                              <p className="text-lg text-grey-950">
-                                {privateOrders.length > 0
-                                  ? privateOrders[privateOrders.length - 1]
-                                      .adultCount
-                                  : "Loading..."}
-                                位大人、
-                                {privateOrders.length > 0
-                                  ? privateOrders[privateOrders.length - 1]
-                                      .childCount
-                                  : "Loading..."}
-                                位小孩
-                              </p>
-                            </div>
-                          </div>
-
-                  
-                          <div className="flex justify-between pt-6">
-                            <p className="text-2xl text-primary-700">
-                              {totalPrice}
+                          {/* 如果沒有訂單，顯示「無私人行程訂單」 */}
+                          {privateOrders.length === 0 ? (
+                            <p className="text-grey-700 py-6 text-center text-xl">
+                              無私人行程訂單
                             </p>
+                          ) : (
+                            <div className="flex gap-4 p-4">
+                              {/* 🔹 使用 .map() 來遍歷所有訂單 */}
+                              {privateOrders.map((order, index) => (
+                                <div
+                                  key={index}
+                                  className="border-1 flex flex-col items-center border border-b border-gray-300 pb-4"
+                                >
+                                  {/* 訂單內容 */}
+                                  <div className="flex space-y-8 px-8 py-6">
+                                    <div className="flex items-center space-x-4">
+                                      <img
+                                        src={
+                                          order.tourguideInfo.imgUrl ||
+                                          "https://images.unsplash.com/photo-1460794418188-1bb7dba2720d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D"
+                                        }
+                                        alt="導遊"
+                                        className="inline-block h-20 w-20 rounded-full"
+                                      />
+                                      <p className="text-xl text-secondary-700">
+                                        {order.tourguideInfo.name || "未知導遊"}
+                                      </p>
+                                    </div>
+                                  </div>
+
+                                  <div className="flex flex-col items-start space-y-2">
+                                    <p className="text-base font-normal text-grey-950">
+                                      日期：{order.selectedDate || "未選擇日期"}
+                                    </p>
+                                    <p className="text-base font-normal text-grey-950">
+                                      時段：{order.selectedSlot || "未選擇時段"}
+                                    </p>
+                                    <p className="text-base font-normal text-grey-950">
+                                      人數: {order.adultCount || 0} 位大人、
+                                      {order.childCount || 0} 位小孩
+                                    </p>
+                                    <p className="text-base font-normal text-grey-950">
+                                      主題：
+                                      {order.selectedTheme || "未選擇主題 "}
+                                    </p>
+                                  </div>
+
+                                  {/* 訂單價格 */}
+                                  {/* <p className="text-2xl text-primary-700 pt-4">
+                {totalPrice ? `${totalPrice} ` : "價格計算中..."}
+              </p> */}
+                                  <p className="ml-auto mr-4 pt-4 text-xl text-primary-700">
+                                    {" "}
+                                    {getPrivateOrderPrice(order)}
+                                  </p>
+                                </div>
+                              ))}
+                            </div>
+                          )}
+
+                          {/* 關閉按鈕 */}
+                          <div className="flex justify-center pt-6">
                             <button
-                              className="rounded-lg bg-red-500 px-4 py-2 text-white"
+                              className="rounded-lg bg-red-500 px-4 py-2 text-base text-white"
                               onClick={() => setPrivateOrderOpen(false)}
                             >
                               關閉
@@ -325,101 +366,7 @@ function PaymentPage() {
                           </div>
                         </div>
                       </div>
-                    )} */}
-
-                    {/* 私人行程訂單 Modal */}
-{isPrivateOrderOpen && (
-  <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-    <div className="w-4/5 max-w-2xl rounded-lg bg-white p-6">
-
-
-
-
-      <h3 className="border-b-4 border-secondary-200 text-2xl text-secondary-500 flex justify-between">
-        私人行程訂單
-          
-      <span className="text-2xl text-primary-700 pt-4"> 小計:
-
-      {subtotalPrivateOrders}  €
-
-
-                {/* {totalPrice ? `${totalPrice} ` : "價格計算中..."} */}
-    </span>
-      </h3>
-      {/* 如果沒有訂單，顯示「無私人行程訂單」 */}
-      {privateOrders.length === 0 ? (
-        <p className="text-center text-xl text-grey-700 py-6">
-          無私人行程訂單
-        </p>
-      ) : (
-        <div className="flex p-4 gap-4">
-          {/* 🔹 使用 .map() 來遍歷所有訂單 */}
-          {privateOrders.map(
-            
-            
-            
-            (order, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center border-b border-gray-300 pb-4 border border-1"
-            >
-              {/* 訂單內容 */}
-              <div className="flex   space-y-8 px-8 py-6">
-                <div className="flex items-center space-x-4">
-                  <img
-                    src={order.tourguideInfo.imgUrl || "https://images.unsplash.com/photo-1460794418188-1bb7dba2720d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D"}
-                    alt="導遊"
-                    className="inline-block h-20 w-20 rounded-full"
-                  />
-                  <p className="text-xl text-secondary-700">
-                    {order.tourguideInfo.name || "未知導遊"}
-                  </p>
-                </div>
-              </div>
-
-              <div className="flex flex-col items-start space-y-2">
-                <p className="text-base font-normal text-grey-950">
-                  日期：{order.selectedDate || "未選擇日期"}
-                </p>
-                <p className="text-base font-normal text-grey-950">
-                  時段：{order.selectedSlot || "未選擇時段"}
-                </p>
-                <p className="text-base font-normal text-grey-950">
-                  人數: {order.adultCount || 0} 位大人、
-                  {order.childCount || 0} 位小孩
-                </p>
-                <p className="text-base font-normal text-grey-950">
-                  主題：{order.selectedTheme || "未選擇主題 "}
-                </p>
-              </div>
-
-              {/* 訂單價格 */}
-              {/* <p className="text-2xl text-primary-700 pt-4">
-                {totalPrice ? `${totalPrice} ` : "價格計算中..."}
-              </p> */}
-              <p className="text-xl text-primary-700 pt-4 ml-auto mr-4"> {getPrivateOrderPrice(order)}</p>
-            </div>
-          ))
-          
-          
-          
-          }
-        </div>
-      )}
-
-      {/* 關閉按鈕 */}
-      <div className="flex justify-center pt-6">
-        <button
-          className="rounded-lg bg-red-500 px-4 py-2 text-white text-base"
-          onClick={() => setPrivateOrderOpen(false)}
-        >
-          關閉
-        </button>
-      </div>
-    </div>
-  </div>
-)}
-
+                    )}
                   </div>
 
                   <div className="flex flex-col gap-2">
@@ -524,7 +471,9 @@ function PaymentPage() {
 
             <div className="flex justify-between px-2 py-6">
               <h4 className="text-xl font-black text-primary-700">總計</h4>
-              <p className="text-xl font-black text-primary-700">880 €</p>
+              <p className="text-xl font-black text-primary-700">
+                {subtotalPrivateOrders} €
+              </p>
             </div>
             <div className="my-20 items-center space-y-4">
               <button
