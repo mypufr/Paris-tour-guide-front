795b1ced867386fe0ebeec41de2aea5ea25a4426
MeiyinPU <mei-yin.pu@oclock.school>
2025-04-21 18:58:40 +0200

    wip: update vite.config.js for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/UI_Paris/':'/',
+  // base: process.env.NODE_ENV === 'production' ? '/UI_Paris/':'/',
+
+  base: '/', // for Render deployment
   plugins: [react()],
   build: {
     rollupOptions: {
